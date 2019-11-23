368f95aa78904e4d5a921bc02296e67744046361
Pythocrates <23015037+Pythocrates@users.noreply.github.com>
2019-11-24 00:07:00 +0100

    Removed old methods. Harmonized low and high level.

--- high_level/app.py
+++ high_level/app.py
@@ -1,43 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from ..low_level.session import Session
-
 
 class App:
-    def __init__(self, session, *args, **kwargs):
+    def __init__(self, wrapper, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__session = session
+        self.__wrapper = wrapper
 
     @property
-    def session(self):
-        return self.__session
-
-    @classmethod
-    def from_environment_cookie(cls, host):
-        session = Session.from_environment_cookie(host=host)
-        return cls.from_session(session=session)
-
-    @classmethod
-    def from_chromium(cls, host):
-        session = Session.from_chromium(host=host)
-        return cls.from_session(session=session)
-
-    @classmethod
-    def from_firefox(cls, host):
-        session = Session.from_firefox(host=host)
-        return cls.from_session(session=session)
-
-    @classmethod
-    def from_kerberos(cls, host):
-        session = Session.from_kerberos(host=host)
-        return cls.from_session(session=session)
-
-    @classmethod
-    def from_functional_user(cls, username, password, host):
-        session = Session.from_functional_user(
-            username=username, password=password, host=host)
-        return cls(session=session)
-
-    @classmethod
-    def from_session(cls, session):
-        return cls(session=session)
+    def _wrapper(self):
+        return self.__wrapper
--- high_level/wiki.py
+++ high_level/wiki.py
@@ -12,29 +12,28 @@ from .app import App
 class Wiki(App):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__confluence = self.session.wiki
 
     @property
     def raw(self):
         '''
         Get the low-level wiki interface.
         '''
-        return self.session.wiki
+        return self._wrapper
 
     def get_space(self, key):
-        return Space(key=key, api=self.__confluence)
+        return Space(key=key, api=self._wrapper)
 
     def get_page_by_tinyui(self, tinyui):
         page_id = struct.unpack(
             'L', base64.b64decode(
                 tinyui.replace('-', '/').ljust(11, 'A').ljust(12, '=')))[0]
-        return Page(id_=page_id, api=self.__confluence)
+        return Page(id_=page_id, api=self._wrapper)
 
     def get_attachment_by_id(self, att_id):
-        return Attachment(id_='att{i}'.format(i=att_id), api=self.__confluence)
+        return Attachment(id_='att{i}'.format(i=att_id), api=self._wrapper)
 
     def __getitem__(self, key):
         return self.get_space(key=key)
 
     def read_uri(self, uri):
-        return self.session.read(uri)
+        return self._wrapper.read(uri)

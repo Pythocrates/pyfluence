9e92f1c64d393ea59588f5ca4ff721e5465ff57f
Pythocrates <23015037+Pythocrates@users.noreply.github.com>
2019-11-24 11:47:00 +0100

    Harmonized Bitbucket high- and low-level wrapper.

--- high_level/bitbucket.py
+++ high_level/bitbucket.py
@@ -4,27 +4,9 @@ from .app import App
 
 
 class Bitbucket(App):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.__confluence = self.session.confluence
-
     @property
     def raw(self):
         '''
         Get the low-level Bitbucket interface.
         '''
-        return self.session.bitbucket
-
-    @property
-    def raw_keys(self):
-        '''
-        Get the low-level Bitbucket keys interface.
-        '''
-        return self.session.bitbucket_keys
-
-    @property
-    def raw_ssh(self):
-        '''
-        Get the low-level Bitbucket ssh interface.
-        '''
-        return self.session.bitbucket_ssh
+        return self._wrapper
